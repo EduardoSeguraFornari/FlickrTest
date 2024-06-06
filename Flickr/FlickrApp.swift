78fef9fde77019007db21bff8e40f91f57ee9390
EduardoSeguraFornari <eduardo.fornari@icloud.com>
2024-06-06 16:15:36 -0300

    Initial Commit

---
+++ Flickr/FlickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrApp.swift
+//  Flickr
+//
+//  Created by Eduardo Fornari on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
